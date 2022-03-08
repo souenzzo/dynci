8577971cc2facc34f2fa8a44ccd2cd824673a929
Enzzo Cavallo <souenzzo@gmail.com>
2022-03-08 08:49:07 -0300

    Using dynamic ci

--- projects/app/src/app/main.clj
+++ projects/app/src/app/main.clj
@@ -2,6 +2,15 @@
   (:require [clj-yaml.core :as yaml]))
 
 
+(def dynjob
+  (let [job {:docker {:image "clojure:tools-deps"}
+             :steps  ["checkout"]}]
+    {:version   "2.1"
+     :jobs      {:build (update job :steps conj {:run "echo hello"})
+                 :test  (update job :steps conj {:run "echo world"})}
+     :workflows {:build_and_test {:jobs [:build :test]}}}))
+
+
 (defn -main
   [& _]
-  (prn :hello))
+  (yaml/generate-stream *out* dynjob))
