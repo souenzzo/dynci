ba5d1f5d9d844ef4f2f22ee78b79aa87c8552d8d
Enzzo Cavallo <souenzzo@gmail.com>
2022-03-08 09:04:40 -0300

    gen via clojure

--- projects/app/src/app/main.clj
+++ projects/app/src/app/main.clj
@@ -1,5 +1,6 @@
 (ns app.main
-  (:require [clj-yaml.core :as yaml]))
+  (:require [clj-yaml.core :as yaml]
+            [clojure.java.io :as io]))
 
 
 (def dynjob
@@ -11,6 +12,29 @@
      :workflows {:build_and_test {:jobs [:build :test]}}}))
 
 
+(def static-config
+  {:version   "2.1"
+   :setup     true
+   :orbs      {:continuation "circleci/continuation@0.1.2"}
+   :jobs      {:setup {:docker [{:image "clojure:tools-deps"}]
+                       :steps  ["checkout"
+                                {:run "apt-get update && apt-get install -y curl jq && rm -rf /var/lib/apt/lists/*"}
+                                {:run {:name    "Generate config"
+                                       :command "cd projects/app && clojure -M --report stderr -m app.main > generated_config.yml"}}
+                                {"continuation/continue" {:configuration_path "projects/app/generated_config.yml"}}]}}
+   :workflows {:setup {:jobs [:setup]}}})
+
 (defn -main
   [& _]
   (yaml/generate-stream *out* dynjob))
+
+
+(defn static
+  []
+  (with-open [config (io/writer (io/file ".." ".." ".circleci" "config.yml"))]
+    (yaml/generate-stream config static-config)))
+
+(comment
+ (static))
+
+
